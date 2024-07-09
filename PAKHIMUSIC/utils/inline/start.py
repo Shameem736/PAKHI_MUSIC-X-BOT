1718b13ad67370d9c03d5c3bad945ce99badebd1
MOONLIGHTXD92 <moonlight92649@gmail.com>
2024-07-09 09:17:58 +0530

    Update start.py

--- PAKHIMUSIC/utils/inline/start.py
+++ PAKHIMUSIC/utils/inline/start.py
@@ -30,6 +30,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
+            InlineKeyboardButton(text=_["S_B_7"], callback_data="gib_source"),
         ],
     ]
     return buttons
